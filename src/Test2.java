88dea54f5e0ee65fc04694ad81c9c8dbf158013a
Isa <isaismzade@gmail.com>
2024-06-17 18:49:08 +0400

    .

--- src/Test2.java
+++ src/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         int[] array = new int[]{3, 6, 9, 3, 8, 2};
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
                 int temp = array[i + 1];
                 array[i + 1] = array[i];
