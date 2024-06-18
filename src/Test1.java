30b3de0a1aab73b52a4600f64cb5c0e3f6a45964
Isa <isaismzade@gmail.com>
2024-06-18 16:34:52 +0400

    .

--- src/Test1.java
+++ src/Test1.java
@@ -1,8 +1,15 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Test1 {
-    public static void main(String[] args) {
+import static java.util.Arrays.binarySearch;
 
+public class Test1 {
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+    public static void main(String[] args) {
+        int a = binarySearch(array, element);
+        if (a >= 0) {
+            System.out.println(true);
+        } else System.out.println(false);
     }
 }
