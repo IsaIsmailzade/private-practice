2a94678d2315c2cf450a4a938cbcec06c6eaf991
Isa <isaismzade@gmail.com>
2024-06-19 12:43:50 +0400

    .

--- src/Test1.java
+++ src/Test1.java
@@ -4,9 +4,6 @@ public class Test1 {
     public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     public static int element = 5;
     public static void main(String[] args) {
-        int a = binarySearch(array, element);
-        if (a >= 0) {
-            System.out.println(true);
-        } else System.out.println(false);
+
     }
 }
