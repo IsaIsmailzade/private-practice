185289a9cf8f276a019a2bab9a7d0d54e7b3a777
Isa <isaismzade@gmail.com>
2024-07-03 13:36:36 +0400

    .

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,5 @@
 public class Test1 {
     public static void main(String[] args) {
-        int intValue = Integer.parseInt(args[0]);
-        System.out.println(intValue);
+        int intValue;
     }
 }
