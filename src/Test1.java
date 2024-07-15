bd41cf5eb0f7ee78800dbabf5a2f1aa6b05cde49
Isa <isaismzade@gmail.com>
2024-07-15 14:11:30 +0400

    .

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,13 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println(getMinFromFour(5, 9, 2, 0));
+    }
+
+    public static int getMinFromTwo(int x, int y) {
+        return Math.min(x, y);
+    }
+
+    public static int getMinFromFour(int x, int y, int m, int n) {
+        return Math.min(getMinFromTwo(x, y), getMinFromTwo(m, n));
     }
 }
