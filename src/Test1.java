eb4fa2ae6a857e4276733721fca39bdee62b074e
Isa <isaismzade@gmail.com>
2024-07-16 19:35:39 +0400

    .

--- src/Test1.java
+++ src/Test1.java
@@ -1,4 +1,12 @@
 public class Test1 {
     public static void main(String[] args) {
+        int[] testArray = {5, 7, 3, 7, 2};
+        fillArrayAndPrint(testArray);
+    }
 
+    static void fillArrayAndPrint(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * i;
+        }
+    }
 }
